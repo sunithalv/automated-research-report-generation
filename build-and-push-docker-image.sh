26e26d83db415130360a988fce4da8e5872409ef
sunithalv <sunithalv05@gmail.com>
2025-12-11 21:22:28 +0400

    latest changes

--- build-and-push-docker-image.sh
+++ build-and-push-docker-image.sh
@@ -9,6 +9,7 @@ APP_ACR_NAME="researchreportacr26"
 IMAGE_NAME="research-report-app"
 TAG="${1:-latest}"
 
+
 echo "Building Docker image for Research Report Generation System..."
 echo "Tag: $TAG"
 echo ""
